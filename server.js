801dd1865a492ed147fd59d36e8f1c7098efccc7
bola-pro-server <gabrielsita892@gmail.com>
2026-08-21 04:28:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("⚽ Bola Pro está funcionando!");
+});
+
+app.listen(PORT, () => {
+  console.log(`⚽ Bola Pro rodando na porta ${PORT}`);
+});node server.
+  jsnpm install express
